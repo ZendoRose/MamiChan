ff849f04edd56f22c528edc92aee46a8e6d99275
ThisIsInky <87899435+ThisIsInky@users.noreply.github.com>
2021-09-27 18:23:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ npm i axios
 npm i cfonts
 npm i chalk
 npm i cheerio
+npm i escape-string-regexp
+npm i fake-useragent
 npm i fluent-ffmpeg
+npm i multistream
 npm i node-fetch
+npm i request
 npm i yt-search
