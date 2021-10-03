e860be4c7641022fa268beee3c9a19aac6f2058c
ThisIsInky <87899435+ThisIsInky@users.noreply.github.com>
2021-10-03 19:06:55 -0300

    Update inky.js

--- inky.js
+++ inky.js
@@ -859,7 +859,6 @@ const uaangkauuuiiu = checkATMuser(sender)
 const jññño = [`${uaangkauuuiiu}`]
 if (jññño < dineroapostado) return reply('𝐋𝐨 𝐬𝐢𝐞𝐧𝐭𝐨, 𝐝𝐢𝐧𝐞𝐫𝐨 𝐢𝐧𝐬𝐮𝐟𝐢𝐜𝐢𝐞𝐧𝐭𝐞')
 confirmATM(sender, dineroapostado)
-apostWin = 5 *  dineroapostado
 const gpp = ['10','10','10','10','5000']
 const gppp = gpp[Math.floor(Math.random() * gpp.length)]
 piro = `➫ ${botName} 𝐀𝐩𝐮𝐞𝐬𝐭𝐚𝐬
@@ -880,9 +879,9 @@ ${dineroapostado}
 ${pushname}
 
 ➼ 𝐑𝐞𝐬𝐮𝐥𝐭𝐚𝐝𝐨:
-𝐇𝐚𝐬 𝐠𝐚𝐧𝐚𝐝𝐨 $${apostWin}`
+𝐇𝐚𝐬 𝐠𝐚𝐧𝐚𝐝𝐨 $𝟓𝟎𝟎𝟎`
 if (gppp < 90) return reply2(piro)
-addKoinUser(sender, `${apostWin}`)
+addKoinUser(sender, 5000)
 reply2(`${ganadorxd}`)
 break
 
