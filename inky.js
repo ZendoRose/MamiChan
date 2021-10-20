8d3fa802e238bac18a1fa0f8eb8bc7af95c7b7c5
ThisIsInky <87899435+ThisIsInky@users.noreply.github.com>
2021-10-20 15:37:22 -0300

    Update inky.js

--- inky.js
+++ inky.js
@@ -578,8 +578,7 @@ case 'besar':
 if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return reply('𝐓𝐚𝐠𝐮𝐞𝐚 𝐜𝐨𝐧 @ 𝐚𝐥 𝐪𝐮𝐞 𝐬𝐞 𝐥𝐞 𝐯𝐚 𝐚 𝐤𝐢𝐜𝐤𝐞𝐚𝐫')
 mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
 inky.sendMessage(from, fs.readFileSync('./media/sticker/kissAnimeYuri.webp'), sticker, {quoted:mek, sendEphemeral: true})
-inky.sendMessage(from, `@${mentioned[0].split('@')[0]} 𝐡𝐚 𝐬𝐢𝐝𝐨 𝐛𝐞𝐬𝐚𝐝𝐨 𝐩𝐨𝐫 @${sender.split("@")[0]}`, text, {
-quoted: mek, sendEphemeral: true, contextInfo: {"mentionedJid": [mentioned]}
+mentions(`@${mentioned[0].split('@')[0]} 𝐡𝐚 𝐬𝐢𝐝𝐨 𝐛𝐞𝐬𝐚𝐝𝐨 𝐩𝐨𝐫 @${sender.split("@")[0]}`, mentioned, true)
 })
 break
 
